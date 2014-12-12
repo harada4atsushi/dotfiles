12b60a1ae7ee5bf3527d98a9e8cf743a8402431f
a.harada <redhornet96@gmail.com>
2014-12-12 11:48:27 +0900

    .pryrcを追加

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #DOT_FILES=( .zsh .zshrc .zshrc.custom .zshrc.alias .zshrc.linux .zshrc.osx .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .vim .vrapperrc import.scala .tmux.conf .dir_colors .rdebugrc .pryrc .percol.d)
-DOT_FILES=( .alias .vimrc .vim .tmux.conf .gitconfig )
+DOT_FILES=( 
+.alias
+.vimrc
+.vim
+.tmux.conf
+.gitconfig
+.pryrc
+)
 
 for file in ${DOT_FILES[@]}
 do
